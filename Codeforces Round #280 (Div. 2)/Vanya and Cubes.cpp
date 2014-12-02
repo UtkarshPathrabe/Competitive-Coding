30f292d3aa12479d59f4fce826b2d9c5a598797a
UtkarshPathrabe <uapathrabe@gmail.com>
2014-12-02 12:03:11 +0530

    Final Commit

---
+++ Codeforces Round #280 (Div. 2)/Vanya and Cubes.cpp
@@ -0,0 +1,17 @@
+/* Question can be found at http://codeforces.com/contest/492/problem/A */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	int n, height = 0, i = 1;
+	scanf("%d", &n);
+	while(n >= ((i * (i + 1)) / 2)) {
+		n -= ((i * (i + 1)) / 2);
+		i += 1;
+		height += 1;
+	}
+	printf("%d\n", height);
+	return 0;
+}
---
+++ Codeforces Round #280 (Div. 2)/Vanya and Field.cpp
@@ -0,0 +1,28 @@
+/* Question can be found at http://codeforces.com/contest/492/problem/E */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M, dx, dy;
+int indexCount[1000005];
+
+inline int add(int x, int y) { return (x + y) % N; }
+inline int mul(int x, int y) { return (long long int)x * y % N; }
+
+int main() {
+	int x, y;
+	cin >> N >> M >> dx >> dy;
+	for(int i = 1; i < N; i++) {
+		if(mul(dy, i) == (N - 1)) {
+			dx = mul(dx, i);
+			break;
+		}
+	}
+	while(M--) {
+		cin >> x >> y;
+		indexCount[add(x, mul(dx, y))]++;
+	}
+	printf("%ld 0\n", max_element(indexCount, indexCount + N) - indexCount);
+	return 0;
+}
