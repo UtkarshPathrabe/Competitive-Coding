d71a3e33f96c7431ba80f776433200a4bd5acea1
UtkarshPathrabe <uapathrabe@gmail.com>
2015-02-04 17:25:06 +0530

    Final Commit

--- Model Algorithms/Dynamic Programming/Knapsack/Unbounded KanpSack.cpp
+++ Model Algorithms/Dynamic Programming/Knapsack/Unbounded KanpSack.cpp
@@ -1,3 +1,14 @@
+/* Author: Utkarsh Ashok Pathrabe
+*  Algorithm: Getting the Unbounded Knapsack value using Tabulation (Bottom Up)
+*/
+
+/* Problem Statement: Given weights and values of n items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. In other words, given two integer
+*  arrays val[0...n-1] and wt[0...n-1] which represent values and weights associated with n items respectively, also given an integer W which represents knapsack capacity, find out the
+*  maximum value of this subset which is smaller than or equal to W. You cannot break an item, either pick the complete item, or don't pick it and assume infinite copies of same item exits.
+*/
+
+/* Time Complexity: O(nW) where n is the number of items and W is the capacity of knapsack. */
+
 #include <bits/stdc++.h>
 
 using namespace std;
