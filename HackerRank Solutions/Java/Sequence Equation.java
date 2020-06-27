6b189bc140f150b0a92102dc82e1e89aecea47af
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-06-27 12:21:33 +0530

    Hackerrank Solution

---
+++ HackerRank Solutions/Java/Sequence Equation.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the permutationEquation function below.
+    static int[] permutationEquation(int[] p) {
+        int result[] = new int[p.length];
+        for (int i = 1; i <= p.length; i++) {
+            int temp = 0;
+            for (int j = 0; j < p.length; j++) {
+                if (p[j] == i) {
+                    temp = j + 1;
+                    break;
+                }
+            }
+            for (int j = 0; j < p.length; j++) {
+                if (p[j] == temp) {
+                    temp = j + 1;
+                    break;
+                }
+            }
+            result[i-1] = temp;
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] p = new int[n];
+
+        String[] pItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pItem = Integer.parseInt(pItems[i]);
+            p[i] = pItem;
+        }
+
+        int[] result = permutationEquation(p);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
