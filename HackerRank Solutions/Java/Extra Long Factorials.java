797e30c66068d21b7ff351bf4b987bc5519a6960
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-06-27 11:13:16 +0530

    Hackerrank Solutions

---
+++ HackerRank Solutions/Java/Extra Long Factorials.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the extraLongFactorials function below.
+    static void extraLongFactorials(int n) {
+        BigInteger factorial = new BigInteger("1");
+        for(int i=2;i<=n;i++){
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(factorial);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
+    }
+}
