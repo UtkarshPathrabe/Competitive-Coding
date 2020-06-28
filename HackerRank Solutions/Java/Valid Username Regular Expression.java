c76a459dc48a65d7380161ec84eb558679da71b5
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-06-28 13:20:52 +0530

    Hackerrank Solution

---
+++ HackerRank Solutions/Java/Valid Username Regular Expression.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class UsernameValidator {
+    public static final String regularExpression = "^[[A-Z]|[a-z]][[A-Z]|[a-z]|\\d|[_]]{7,29}$";
+}
+
+
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
+}
