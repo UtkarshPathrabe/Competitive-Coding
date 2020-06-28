d1218bc688986a6d151aa9157be0efafdd7f47f8
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-06-28 13:36:35 +0530

    Hackerrank Solution

---
+++ HackerRank Solutions/Java/Java Static Initializer.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+static int B, H;
+static boolean flag = false;
+static {
+    Scanner scan = new Scanner(System.in);
+    B = scan.nextInt();
+    H = scan.nextInt();
+    if (B <= 0 || H <= 0) {
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    } else {
+        flag = true;
+    }
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
