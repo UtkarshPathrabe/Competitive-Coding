40a9d91a8b11917914d44b72c3865ea823fcdcb3
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-07-05 20:27:16 +0530

    Hackerrank Solution

---
+++ HackerRank Solutions/Interview Preparation Kit/Sorting/Mark and Toys.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    count = 0
+    priceSum = 0
+    for i in range(len(prices)):
+        if priceSum + prices[i] <= k:
+            count += 1
+            priceSum += prices[i]
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
