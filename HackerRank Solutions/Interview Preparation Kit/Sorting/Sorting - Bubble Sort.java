b7b802201eb90bb4c70c56be5d34a397289c2963
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-07-05 19:45:43 +0530

    Hackerrank Solution

---
+++ HackerRank Solutions/Interview Preparation Kit/Sorting/Sorting - Bubble Sort.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+        int numberOfSwaps = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j+1]) {
+                    numberOfSwaps++;
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Array is sorted in " + numberOfSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length - 1]);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
+}
