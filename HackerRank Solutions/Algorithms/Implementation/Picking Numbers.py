71c85041b19aca7ba0eded6edf6213fd31a454dd
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-07-11 20:47:54 +0530

    Hackerrank Solution

---
+++ HackerRank Solutions/Algorithms/Implementation/Picking Numbers.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    countMap = Counter(a)
+    return max(countMap[k] + countMap.get(k+1, 0) for k in countMap)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
