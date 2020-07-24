46d5cc794e30a3ddcce4ef1f4c3c339ccaf65d09
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-07-24 10:46:13 +0530

    Create Greedy Florist.py

---
+++ HackerRank Solutions/Interview Preparation Kit/Greedy Algorithms/Greedy Florist.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(k, c):
+    c.sort(reverse=True)
+    amount = 0
+    for i in range(len(c)):
+        amount += (((i//k) + 1) * c[i])
+    return amount
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
