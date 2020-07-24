164f823f756dad9838b07652cd2449d99a42b6d2
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-07-24 11:20:49 +0530

    Create Minimum Absolute Difference in an Array.py

---
+++ HackerRank Solutions/Interview Preparation Kit/Greedy Algorithms/Minimum Absolute Difference in an Array.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    difference = sys.maxsize
+    for i in range(len(arr) - 1):
+        difference = min(difference, arr[i+1] - arr[i])
+    return difference
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
