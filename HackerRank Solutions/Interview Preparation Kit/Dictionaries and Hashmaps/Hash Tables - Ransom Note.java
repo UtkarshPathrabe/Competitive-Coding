7da161e116f27092910efcf01e29264ad7da6476
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-07-25 16:33:22 +0530

    Create Hash Tables - Ransom Note.java

---
+++ HackerRank Solutions/Interview Preparation Kit/Dictionaries and Hashmaps/Hash Tables - Ransom Note.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+        Map<String, Integer> wordFrequency = new HashMap<String, Integer>();
+        for (int i = 0; i < magazine.length; i++) {
+            wordFrequency.put(magazine[i], wordFrequency.getOrDefault(magazine[i], 0) + 1);
+        }
+        for (int i = 0; i < note.length; i++) {
+            if (wordFrequency.containsKey(note[i]) && wordFrequency.get(note[i]) > 0) {
+                wordFrequency.put(note[i], wordFrequency.get(note[i]) - 1);
+            } else {
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
