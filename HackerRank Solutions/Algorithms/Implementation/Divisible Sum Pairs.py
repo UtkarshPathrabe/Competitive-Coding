47897566b3768506225a10fa1760263968090fb0
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-07-31 20:05:24 +0530

    Create Divisible Sum Pairs.py

---
+++ HackerRank Solutions/Algorithms/Implementation/Divisible Sum Pairs.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the divisibleSumPairs function below.
+def divisibleSumPairs(n, k, ar):
+    pairCount = 0
+    for i in range(0, n-1):
+        for j in range(i+1, n):
+            if ((ar[i] + ar[j]) % k == 0):
+                pairCount += 1
+    return pairCount
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
