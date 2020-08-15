936f9b01997b0174a1d6ef0bcd6b131f8853e71a
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-08-15 11:02:08 +0530

    Create Binary Tree Postorder Traversal - Recursive.py

---
+++ LeetCode/Binary Tree/Binary Tree Postorder Traversal - Recursive.py
@@ -0,0 +1,9 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val] if root else []
