2ddef1ca25ed90ba56a134fe0ade4073e626f83c
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-08-16 12:07:11 +0530

    Create Maximum Depth of Binary Tree.py

---
+++ LeetCode/Recursion I/Maximum Depth of Binary Tree.py
@@ -0,0 +1,9 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1 if root else 0
