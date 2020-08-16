f28aa0ed0b6192b24e2ad40b6ff35ad0b6d1aea4
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-08-16 15:59:54 +0530

    Create Validate Binary Search Tree - Recursive.py

---
+++ LeetCode/Binary Search Tree/Validate Binary Search Tree - Recursive.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def helper(node, lower = float('-inf'), upper = float('inf')):
+            if node is None:
+                return True
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+        return helper(root)
