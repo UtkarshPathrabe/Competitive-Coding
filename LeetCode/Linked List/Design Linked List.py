af7e16dd42929be0d4c5a3fde2629bb3d3084a9f
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-08-21 11:13:45 +0530

    Create Design Linked List.py

---
+++ LeetCode/Linked List/Design Linked List.py
@@ -0,0 +1,73 @@
+class Node:
+    
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+
+class MyLinkedList:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.size = 0
+        self.head = Node(0)
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        if index < 0 or index >= self.size:
+            return -1
+        curr = self.head
+        for _ in range(index + 1):
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        self.addAtIndex(0, val)
+        
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        self.addAtIndex(self.size, val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        if index > self.size:
+            return
+        if index < 0:
+            index = 0
+        self.size += 1
+        pred = self.head
+        for _ in range(index):
+            pred = pred.next
+        newNode = Node(val)
+        newNode.next = pred.next
+        pred.next = newNode
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        if index < 0 or index >= self.size:
+            return
+        self.size -= 1
+        pred = self.head
+        for _ in range(index):
+            pred = pred.next
+        pred.next = pred.next.next
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
