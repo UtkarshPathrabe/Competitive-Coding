efc7493f4a447bf8a75f88bc92307e4e1bfc48e0
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-08-22 17:33:36 +0530

    Create My Calendar II.py

---
+++ LeetCode/Mock Interviews/Google/Phone Interview 01/My Calendar II.py
@@ -0,0 +1,19 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.calendar = []
+        self.overlaps = []
+
+    def book(self, start: int, end: int) -> bool:
+        for i, j in self.overlaps:
+            if start < j and end > i:
+                return False
+        for i, j in self.calendar:
+            if start < j and end > i:
+                self.overlaps.append((max(start, i), min(end, j)))
+        self.calendar.append((start, end))
+        return True
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
