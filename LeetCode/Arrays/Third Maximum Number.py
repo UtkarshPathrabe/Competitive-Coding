487d09b3b96e0c311f3e1fa2e932e2ba0b7a0658
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-08-23 11:40:46 +0530

    Create Third Maximum Number.py

---
+++ LeetCode/Arrays/Third Maximum Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        maximums = set()
+        for num in nums:
+            maximums.add(num)
+            if len(maximums) > 3:
+                maximums.remove(min(maximums))
+        if len(maximums) == 3:
+            return min(maximums)
+        return max(maximums)
