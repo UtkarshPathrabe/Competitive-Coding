8960b5c685cb68aee82ee25c03560bd8d01df644
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-08-23 13:03:17 +0530

    Create N-ary Tree Postorder Traversal.py

---
+++ LeetCode/N-ary Tree/N-ary Tree Postorder Traversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        stack, result = [root,], []
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            stack.extend(node.children)
+        return result[::-1]
