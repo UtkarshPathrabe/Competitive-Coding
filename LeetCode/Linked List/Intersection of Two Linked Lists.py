f836b6c1f45ab49330c6d80539a55480a9edd0ba
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-08-24 09:22:17 +0530

    Create Intersection of Two Linked Lists.py

---
+++ LeetCode/Linked List/Intersection of Two Linked Lists.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        pointerA = headA
+        pointerB = headB
+        while pointerA != pointerB:
+            pointerA = pointerA.next if pointerA is not None else headB
+            pointerB = pointerB.next if pointerB is not None else headA
+        return pointerA
