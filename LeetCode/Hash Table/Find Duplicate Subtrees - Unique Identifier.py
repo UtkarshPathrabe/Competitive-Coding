d0aa449f22b7113d30a8dae6455304e444a0cc22
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-09-03 19:59:33 +0530

    Create Find Duplicate Subtrees - Unique Identifier.py

---
+++ LeetCode/Hash Table/Find Duplicate Subtrees - Unique Identifier.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
+        trees = defaultdict()
+        trees.default_factory = trees.__len__
+        count = Counter()
+        result = []
+        
+        def lookup(node):
+            if node:
+                uid = trees[node.val, lookup(node.left), lookup(node.right)]
+                count[uid] += 1
+                if count[uid] == 2:
+                    result.append(node)
+                return uid
+        
+        lookup(root)
+        return result
