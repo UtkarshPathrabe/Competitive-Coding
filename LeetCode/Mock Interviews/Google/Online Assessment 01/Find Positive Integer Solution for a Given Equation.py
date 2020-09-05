f154aeda264ba5b5e8cd19478968ab2465618cd0
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-09-05 11:57:29 +0530

    Create Find Positive Integer Solution for a Given Equation.py

---
+++ LeetCode/Mock Interviews/Google/Online Assessment 01/Find Positive Integer Solution for a Given Equation.py
@@ -0,0 +1,19 @@
+"""
+   This is the custom function interface.
+   You should not implement it, or speculate about its implementation
+   class CustomFunction:
+       # Returns f(x, y) for any given positive integers x and y.
+       # Note that f(x, y) is increasing with respect to both x and y.
+       # i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+       def f(self, x, y):
+  
+"""
+
+class Solution:
+    def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+        result = []
+        for x in range(1, z + 1):
+            for y in range(1, z + 1):
+                if customfunction.f(x, y) == z:
+                    result.append([x, y])
+        return result
