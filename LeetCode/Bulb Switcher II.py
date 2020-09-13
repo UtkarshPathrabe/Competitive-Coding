d731abed59c53e4c0ec6571450a07c77bb79c0c0
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-09-13 16:31:13 +0530

    Create Bulb Switcher II.py

---
+++ LeetCode/Bulb Switcher II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def flipLights(self, n: int, m: int) -> int:
+        n = min(n, 3)
+        if m == 0:
+            return 1
+        elif m == 1:
+            return [2, 3, 4][n - 1]
+        elif m == 2:
+            return [2, 4, 7][n - 1]
+        else:
+            return [2, 4, 8][n - 1]
