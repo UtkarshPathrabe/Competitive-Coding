410374a69396501dce0240a1235381e23330103a
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-09-18 01:35:08 +0530

    Create Day 2 - Loops.js

---
+++ HackerRank Solutions/Tutorials/10 Days of Javascript/Day 2 - Loops.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    for(let i = 0; i < s.length; i++) {
+        if (s[i] === 'a' || s[i] === 'e' || s[i] === 'i' || s[i] === 'o' || s[i] === 'u') {
+            console.log(s[i]);
+        }
+    }
+    for(let i = 0; i < s.length; i++) {
+        if (s[i] !== 'a' && s[i] !== 'e' && s[i] !== 'i' && s[i] !== 'o' && s[i] !== 'u') {
+            console.log(s[i]);
+        }
+    }
+}
+
