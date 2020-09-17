fea9e0d46b710c03f033fc34c044c8fe0434844a
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-09-18 01:45:33 +0530

    Create Day 1 - Let and Const.js

---
+++ HackerRank Solutions/Tutorials/10 Days of Javascript/Day 1 - Let and Const.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    const r = readLine();
+    // Print the area of the circle:
+    console.log(PI * r * r);
+    // Print the perimeter of the circle:
+    console.log(2 * PI * r);
+    try {    
