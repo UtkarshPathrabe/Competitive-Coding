206ee664dae9f39fd034cd45005fbc7fcb590fea
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-09-18 02:02:18 +0530

    Create Functions in C.c

---
+++ HackerRank Solutions/C/Introduction/Functions in C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d) {
+    int max = a;
+    if (b > max) {
+        max = b;
+    }
+    if (c > max) {
+        max = c;
+    }
+    if (d > max) {
+        max = d;
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
