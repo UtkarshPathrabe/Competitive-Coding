502a02a073e1afcb574b9c789e6bd6ef04ec3fc0
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-09-20 18:58:44 +0530

    Create Nested List Weight Sum II.py

---
+++ LeetCode/Nested List Weight Sum II.py
@@ -0,0 +1,56 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        maxDepth, result = 1, []
+        def helper(nestedList, depth):
+            nonlocal maxDepth, result
+            maxDepth = max(maxDepth, depth)
+            for element in nestedList:
+                if not element.isInteger():
+                    helper(element.getList(), depth + 1)
+                else:
+                    result.append((element.getInteger(), depth))
+        helper(nestedList, 1)
+        return sum((maxDepth - depth + 1) * integer for integer, depth in result)
