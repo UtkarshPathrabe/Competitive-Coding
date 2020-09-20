26839b3ab5451b0e8f5e5794674a5335cbe80203
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-09-20 18:58:42 +0530

    Create Nested List Weight Sum.py

---
+++ LeetCode/Nested List Weight Sum.py
@@ -0,0 +1,54 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        def helper(nestedList, depth):
+            currentSum = 0
+            for element in nestedList:
+                if element.isInteger():
+                    currentSum += element.getInteger() * depth
+                else:
+                    currentSum += helper(element.getList(), depth + 1)
+            return currentSum
+        return helper(nestedList, 1)
