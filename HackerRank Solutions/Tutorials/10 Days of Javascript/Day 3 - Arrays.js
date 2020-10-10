091701b89c740c2f808d2f3a19a8272a41e4ef1c
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-10-10 18:42:36 +0530

    Create Day 3 - Arrays.js

---
+++ HackerRank Solutions/Tutorials/10 Days of Javascript/Day 3 - Arrays.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let largest = 0;
+    let secondLargest = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > largest) {
+            secondLargest = largest;
+            largest = nums[i];
+        } else if (nums[i] > secondLargest && nums[i] !== largest) {
+            secondLargest = nums[i];
+        }
+    }
+    return secondLargest;
+}
+
