420f035a7e6c0a02cf5c193d5e4524b6b67a02f0
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-10-27 17:50:50 +0530

    Create Flatten a Multilevel Doubly Linked List.py

---
+++ LeetCode/Linked List/Flatten a Multilevel Doubly Linked List.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        if not head:
+            return
+        pseudoHead = Node(None, None, head, None)
+        prev, stack = pseudoHead, deque([])
+        stack.append(head)
+        while stack:
+            curr = stack.pop()
+            prev.next = curr
+            curr.prev = prev
+            if curr.next:
+                stack.append(curr.next)
+            if curr.child:
+                stack.append(curr.child)
+                curr.child = None
+            prev = curr
+        pseudoHead.next.prev = None
+        return pseudoHead.next
