fab48b06146a4a9ee66b9610bc20e36e01d0910f
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-11-16 13:08:45 +0530

    Create Decompress Run-Length Encoded List.py

---
+++ LeetCode/Decompress Run-Length Encoded List.py
@@ -0,0 +1,6 @@
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in range(0, len(nums), 2):
+            result.extend([nums[i + 1]] * nums[i])
+        return result
