e604f91e842757bd37ead81808ab98d6efc5db23
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-11-20 19:29:54 +0530

    Create Mini Parser.py

---
+++ LeetCode/Mini Parser.py
@@ -0,0 +1,67 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def deserialize(self, s: str) -> NestedInteger:
+        queue = deque(s + '$')
+        def parseInteger():
+            integer, sign = 0, 1
+            if queue[0] == '-':
+                sign = -1
+                queue.popleft()
+            while queue[0].isdigit():
+                integer = integer * 10 + int(queue.popleft())
+            return NestedInteger(sign * integer)
+        def parseNestedInteger():
+            if queue[0] != '[':
+                return parseInteger()
+            queue.popleft()
+            nestedInteger = NestedInteger()
+            if queue[0] != ']':
+                nestedInteger.add(parseNestedInteger())
+                while queue[0] != ']':
+                    queue.popleft()
+                    nestedInteger.add(parseNestedInteger())
+            queue.popleft()
+            return nestedInteger
+        return parseNestedInteger()
