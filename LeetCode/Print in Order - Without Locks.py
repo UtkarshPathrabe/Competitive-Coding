4075eb34c0f68ddb1eda8d587ff7102aa2de041f
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2021-01-30 17:19:04 +0530

    Create Print in Order - Without Locks.py

---
+++ LeetCode/Print in Order - Without Locks.py
@@ -0,0 +1,27 @@
+class Foo:
+    def __init__(self):
+        self.firstExecuted = self.secondExecuted = False
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.firstExecuted = True
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        while not self.firstExecuted:
+            continue
+        printSecond()
+        self.secondExecuted = True
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        while not self.secondExecuted:
+            continue
+        printThird()
