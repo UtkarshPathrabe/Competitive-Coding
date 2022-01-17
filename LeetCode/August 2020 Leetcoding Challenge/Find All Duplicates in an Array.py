628f94680e53bede51904e335653140d10a50841
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-17 19:53:25 +0530

    Delete Find All Duplicates in an Array.py

--- LeetCode/August 2020 Leetcoding Challenge/Find All Duplicates in an Array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findDuplicates(self, nums: List[int]) -> List[int]:
-        result = []
-        for i in range(len(nums)):
-            if nums[abs(nums[i]) - 1] < 0:
-                result.append(abs(nums[i]))
-            else:
-                nums[abs(nums[i]) - 1] *= -1
-        return result
