3c2a1a7d973bdefe9676c7675aec77a384360abc
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-03 19:03:18 +0530

    Create Add and Search Word - 02.py

---
+++ LeetCode/LeetCoding Challenges/Aug 2020 Leetcoding Challenge/Add and Search Word - 02.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    
+    def __init__(self):
+        self.childNodes = {}
+        self.isEnd = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if node.childNodes.get(c) == None:
+                node.childNodes[c] = TrieNode()
+            node = node.childNodes.get(c)
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        def searchInNode(word, node) -> bool:
+            for i, c in enumerate(word):
+                if node.childNodes.get(c) == None:
+                    if c == '.':
+                        for x in node.childNodes:
+                            if searchInNode(word[i+1:], node.childNodes.get(x)):
+                                return True
+                    return False
+                else:
+                    node = node.childNodes.get(c)
+            return node.isEnd
+        return searchInNode(word, self.root)
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
