dd2fc7ce4225b100fbec7f3798e2e50cfb4d9753
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-13 20:04:55 +0530

    Create Number of Longest Increasing Subsequence.py

---
+++ LeetCode/LeetCoding Challenges/2020-10 Leetcoding Challenge/Number of Longest Increasing Subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findNumberOfLIS(self, nums):
+        N = len(nums)
+        if N <= 1: return N
+        lengths = [0] * N #lengths[i] = longest ending in nums[i]
+        counts = [1] * N #count[i] = number of longest ending in nums[i]
+        for j, num in enumerate(nums):
+            for i in range(j):
+                if nums[i] < nums[j]:
+                    if lengths[i] >= lengths[j]:
+                        lengths[j] = 1 + lengths[i]
+                        counts[j] = counts[i]
+                    elif lengths[i] + 1 == lengths[j]:
+                        counts[j] += counts[i]
+        longest = max(lengths)
+        return sum(c for i, c in enumerate(counts) if lengths[i] == longest)
