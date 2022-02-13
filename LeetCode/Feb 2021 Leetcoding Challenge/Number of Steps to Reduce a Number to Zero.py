1e38f626fed5dfdbafbe611055f497f445ee7685
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-13 19:43:48 +0530

    Delete Number of Steps to Reduce a Number to Zero.py

--- LeetCode/Feb 2021 Leetcoding Challenge/Number of Steps to Reduce a Number to Zero.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def numberOfSteps (self, num: int) -> int:
-        binary = bin(num)[2:]
-        ones = binary.count("1")
-        total = len(binary)
-        return ones + total - 1
