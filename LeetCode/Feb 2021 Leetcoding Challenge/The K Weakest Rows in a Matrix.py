546d16cc15d5ead0dc15c165f9b389fb78f2d075
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-13 19:44:11 +0530

    Delete The K Weakest Rows in a Matrix.py

--- LeetCode/Feb 2021 Leetcoding Challenge/The K Weakest Rows in a Matrix.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        result, soldierFreq = [], []
-        for index, row in enumerate(mat):
-            soldierFreq.append((row.count(1), index))
-        soldierFreq.sort()
-        return [i for freq, i in soldierFreq[:k]]
