8e595bc6827a1769f3b95702b484169389893f6e
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-24 18:24:47 +0530

    Delete Sum of Square Numbers.py

--- LeetCode/LeetCoding Challenges/Aug 2021 LeetCoding Challenge/Sum of Square Numbers.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def judgeSquareSum(self, c: int) -> bool:
-        a = 0
-        while a * a <= c:
-            b = math.sqrt(c - int(a * a))
-            if b == int(b):
-                return True
-            a += 1
-        return False
