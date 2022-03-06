6d911597e52c26d1670b4277e6a35b4fd40aa7c4
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-06 19:04:09 +0530

    Create Swap Nodes in Pairs.py

---
+++ LeetCode/LeetCoding Challenges/2022-02 Leetcoding Challenge/Swap Nodes in Pairs.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        dummyHead.next = head
+        prevNode = dummyHead
+        currentNode = dummyHead.next
+        while currentNode and currentNode.next:
+            firstNode = currentNode
+            secondNode = currentNode.next
+            prevNode.next = secondNode
+            firstNode.next = secondNode.next
+            secondNode.next = firstNode
+            prevNode = firstNode
+            currentNode = firstNode.next
+        return dummyHead.next
