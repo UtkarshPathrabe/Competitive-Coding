924f8b4b81b485a29b744cbd6eeb861d5c7192ef
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-04-15 10:01:08 +0530

    Create Trim a Binary Search Tree.py

---
+++ LeetCode/April 2022 Leetcoding Challenge/Trim a Binary Search Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], L: int, R: int) -> Optional[TreeNode]:
+        def trim(node):
+            if not node:
+                return None
+            elif node.val > R:
+                return trim(node.left)
+            elif node.val < L:
+                return trim(node.right)
+            else:
+                node.left = trim(node.left)
+                node.right = trim(node.right)
+                return node
+        return trim(root)
