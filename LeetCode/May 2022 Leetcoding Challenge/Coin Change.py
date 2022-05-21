dfd786a7bb0c210c217ed3994a069dcf1faadc89
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-05-21 21:02:06 +0530

    Create Coin Change.py

---
+++ LeetCode/May 2022 Leetcoding Challenge/Coin Change.py
@@ -0,0 +1,8 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1
