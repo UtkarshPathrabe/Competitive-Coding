c49c925e5378218319933465270a96608ad0c6dd
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-07-11 10:21:33 +0530

    Create Binary Tree Right Side View.py

---
+++ LeetCode/July 2022 Leetcoding Challenge/Binary Tree Right Side View.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return []
+        queue = deque([root,])
+        result = []
+        while queue:
+            levelSize = len(queue)
+            for i in range(levelSize):
+                node = queue.popleft()
+                if i == levelSize - 1:
+                    result.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return result
