d8d50b48b8c151c7022b0cfb75185923433c9f1d
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-08-02 08:13:11 +0530

    Create Prefix and Suffix Search.py

---
+++ LeetCode/LeetCoding Challenges/2022-06 Leetcoding Challenge/Prefix and Suffix Search.py
@@ -0,0 +1,28 @@
+Trie = lambda: defaultdict(Trie)
+WEIGHT = False
+
+class WordFilter:
+
+    def __init__(self, words: List[str]):
+        self.trie = Trie()
+        for weight, word in enumerate(words):
+            word += '#'
+            for i in range(len(word)):
+                cur = self.trie
+                cur[WEIGHT] = weight
+                for j in range(i, 2 * len(word) - 1):
+                    cur = cur[word[j % len(word)]]
+                    cur[WEIGHT] = weight
+
+    def f(self, prefix: str, suffix: str) -> int:
+        cur = self.trie
+        for letter in suffix + '#' + prefix:
+            if letter not in cur:
+                return -1
+            cur = cur[letter]
+        return cur[WEIGHT]
+
+
+# Your WordFilter object will be instantiated and called as such:
+# obj = WordFilter(words)
+# param_1 = obj.f(prefix,suffix)
