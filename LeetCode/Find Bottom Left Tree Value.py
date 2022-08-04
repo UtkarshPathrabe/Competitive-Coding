4358629e1066ed1adff591e0fab347a755261c0c
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-08-04 20:40:28 +0530

    Create Find Bottom Left Tree Value.py

---
+++ LeetCode/Find Bottom Left Tree Value.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        queue, result = deque([root,]), root.val
+        while queue:
+            level, levelLen = [], len(queue)
+            for i in range(levelLen):
+                node = queue.popleft()
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                result = level[0]
+        return result
