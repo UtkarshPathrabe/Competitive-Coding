c200fc8a4f0229573fce7e165b12f1d4f9c657c8
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-08-06 14:58:48 +0530

    Delete Count of Smaller Numbers After Self.py

--- LeetCode/2022-07 Leetcoding Challenge/Count of Smaller Numbers After Self.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countSmaller(self, nums: List[int]) -> List[int]:
-        numsLength = len(nums)
-        result = [0 for _ in range(numsLength)]
-        cache = []
-        for i in range(numsLength - 1, -1, -1):
-            insertionIndex = result[i] = bisect_left(cache, nums[i])
-            cache.insert(insertionIndex, nums[i])
-        return result
