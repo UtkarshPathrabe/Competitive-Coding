7b7fbaf8e03d2f8c11277c52ba278352fe6192ef
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-08-06 12:19:26 +0530

    Update Poor Pigs.py

--- LeetCode/August 2022 Leetcoding Challenge/Poor Pigs.py
+++ LeetCode/August 2022 Leetcoding Challenge/Poor Pigs.py
@@ -1,4 +1,4 @@
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
         states = (minutesToTest // minutesToDie) + 1
-        return math.ceil(math.log(buckets) / math.log(states))
+        return math.ceil(math.log(buckets, states))
