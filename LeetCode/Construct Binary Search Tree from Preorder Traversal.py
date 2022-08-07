6a9255470e383267806e16ca3efd4cd0b111e67a
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-08-07 18:50:48 +0530

    Create Construct Binary Search Tree from Preorder Traversal.py

---
+++ LeetCode/Construct Binary Search Tree from Preorder Traversal.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        def constructBST(l: int, r: int) -> Optional[TreeNode]:
+            if l < r:
+                splitIndex = l + 1
+                while splitIndex < r and preorder[splitIndex] < preorder[l]:
+                    splitIndex += 1
+                return TreeNode(preorder[l], constructBST(l + 1, splitIndex), constructBST(splitIndex, r))
+            return None
+        return constructBST(0, len(preorder))
