93dc2566bea300677de08433bf5f5ca4ea526585
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-08-20 11:59:50 +0530

    Update Find Resultant Array After Removing Anagrams.py

--- LeetCode/Find Resultant Array After Removing Anagrams.py
+++ LeetCode/Find Resultant Array After Removing Anagrams.py
@@ -3,11 +3,10 @@ class Solution:
         result, index = [], 0
         while index < len(words):
             result.append(words[index])
+            sortedWord = sorted(words[index])
             increment = 1
-            map1 = Counter(words[index])
             while (index + increment) < len(words):
-                map2 = Counter(words[index + increment])
-                if map1 != map2:
+                if sortedWord != sorted(words[index + increment]):
                     break
                 increment += 1
             index += increment
