74d029911c6bc6fabc3f5ede8092e42f0e8d83c0
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-09-07 17:04:41 +0530

    Delete Mirror Reflection.py

--- LeetCode/LeetCoding Challenges/August 2022 Leetcoding Challenge/Mirror Reflection.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def mirrorReflection(self, p: int, q: int) -> int:
-        g = math.gcd(p, q)
-        p, q = (p / g) % 2, (q / g) % 2
-        return 1 if p and q else 0 if p else 2
