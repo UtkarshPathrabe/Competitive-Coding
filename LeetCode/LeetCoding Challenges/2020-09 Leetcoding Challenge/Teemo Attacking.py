0cc79d23625fc28b176b4ee80decc741be90d40d
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-02 11:49:18 +0530

    Delete Teemo Attacking.py

--- LeetCode/LeetCoding Challenges/2020-09 Leetcoding Challenge/Teemo Attacking.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
-        if len(timeSeries) == 0:
-            return 0
-        poisonedDuration = 0
-        for i in range(1, len(timeSeries)):
-            poisonedDuration += min(timeSeries[i] - timeSeries[i - 1], duration)
-        return poisonedDuration + duration
