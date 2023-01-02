40cc233cc0fce72338c7e3a7ff92f1b1142c75ba
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-02 11:39:04 +0530

    Create Longest Common Subsequence.py

---
+++ LeetCode/LeetCoding Challenges/December 2022 Leetcoding Challenge/Longest Common Subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[len(text1)][len(text2)]
