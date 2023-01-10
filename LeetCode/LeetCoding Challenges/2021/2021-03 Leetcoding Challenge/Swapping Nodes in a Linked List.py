22ad476d30221183f355a4969185440873d0bb48
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-10 21:18:52 +0530

    Create Swapping Nodes in a Linked List.py

---
+++ LeetCode/LeetCoding Challenges/2021/2021-03 Leetcoding Challenge/Swapping Nodes in a Linked List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        listLength, frontNode, endNode, currentNode = 0, None, None, head
+        while currentNode != None:
+            listLength += 1
+            if endNode != None:
+                endNode = endNode.next
+            if listLength == k:
+                frontNode = currentNode
+                endNode = head
+            currentNode = currentNode.next
+        frontNode.val, endNode.val = endNode.val, frontNode.val
+        return head
