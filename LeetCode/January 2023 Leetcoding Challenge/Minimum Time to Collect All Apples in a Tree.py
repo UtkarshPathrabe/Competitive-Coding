e45dcc389a2b2ae310b466ea6b3752c2cc127031
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-11 22:43:14 +0530

    Create Minimum Time to Collect All Apples in a Tree.py

---
+++ LeetCode/January 2023 Leetcoding Challenge/Minimum Time to Collect All Apples in a Tree.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        graph = defaultdict(set)
+        for start, end in edges:
+            graph[start].add(end)
+            graph[end].add(start)
+        def dfs(node, parent):
+            if node not in graph:
+                print(node, parent, 0, 'lol')
+                return 0
+            totalTime, childTime = 0, 0
+            for neighbour in graph[node]:
+                if neighbour == parent:
+                    continue
+                childTime = dfs(neighbour, node)
+                # childTime > 0 indicates subtree of child has apples. Since the root node of the
+                # subtree does not contribute to the time, even if it has an apple, we have to check it
+                # independently.
+                if childTime > 0 or hasApple[neighbour]:
+                    totalTime += (childTime + 2)
+            print(node, parent, totalTime)
+            return totalTime
+        return dfs(0, -1)
