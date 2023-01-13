65955b7dd2f1574d1e41f6419e8d3e1a8248790c
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-13 23:44:30 +0530

    Create Single Number II.py

---
+++ LeetCode/Single Number II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        def checkBit(num, i):
+            return (num >> i) & 1 == 1                      # return positive value as is
+        for i in range(32):
+            bitCount = 0
+            for num in nums:
+                if checkBit(num, i):
+                    bitCount += 1
+            if bitCount % 3 == 1:
+                result |= (1 << i)
+        return result - 2 * (result & (1<<31))
