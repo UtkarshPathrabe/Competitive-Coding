855a9a0e2bc6c0c4526a37a08e58327baba7aa54
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-23 18:35:49 +0530

    Delete Complex Number Multiplication.py

--- LeetCode/LeetCoding Challenges/2021-08 LeetCoding Challenge/Complex Number Multiplication.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def complexNumberMultiply(self, a: str, b: str) -> str:
-        aReal, aImg, bReal, bImg = int(a.split('+')[0]), int(a.split('+')[1][:-1]), int(b.split('+')[0]), int(b.split('+')[1][:-1])
-        return str((aReal * bReal) - (aImg * bImg)) + '+' + str((aReal * bImg) + (bReal * aImg)) + 'i'
