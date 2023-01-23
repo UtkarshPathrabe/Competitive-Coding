355e7c5f92249f05f25b6301ee2f36dd7848844b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-01-23 18:36:41 +0530

    Delete Two Sum.py

--- LeetCode/LeetCoding Challenges/2021-08 LeetCoding Challenge/Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numberMap = dict()
-        for i, num in enumerate(nums):
-            complementNumber = target - num
-            if complementNumber in numberMap:
-                return [numberMap[complementNumber], i]
-            numberMap[num] = i
-        return []
