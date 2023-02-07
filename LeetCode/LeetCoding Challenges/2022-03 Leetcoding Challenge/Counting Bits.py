8a6214a8823498c4a49cded72eb7c04649d6d097
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-02-07 19:27:56 +0530

    Delete Counting Bits.py

--- LeetCode/LeetCoding Challenges/2022-03 Leetcoding Challenge/Counting Bits.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countBits(self, num: int) -> List[int]:
-        result, i, b = [0] * (num + 1), 0, 1
-        while b <= num:
-            while i < b and i + b <= num:
-                result[i + b] = result[i] + 1
-                i += 1
-            i, b = 0, b << 1
-        return result
