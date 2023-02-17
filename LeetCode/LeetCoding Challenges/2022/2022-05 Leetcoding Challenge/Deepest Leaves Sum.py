de8696d5c73aa17b579fbf59b14a464c01d848af
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-02-17 18:53:10 +0530

    Create Deepest Leaves Sum.py

---
+++ LeetCode/LeetCoding Challenges/2022/2022-05 Leetcoding Challenge/Deepest Leaves Sum.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        nextLevel = deque([root,])
+        while nextLevel:
+            currentLevel, nextLevel = nextLevel, deque()
+            for node in currentLevel:
+                if node.left:
+                    nextLevel.append(node.left)
+                if node.right:
+                    nextLevel.append(node.right)
+        return sum([node.val for node in currentLevel])
