512ca42bee4b6c4eb605d64e413de9ca227bedde
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-02-19 22:05:29 +0530

    Create H-Index II (Binary Search).py

---
+++ LeetCode/H-Index II (Binary Search).py
@@ -0,0 +1,11 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        left, right = 0, n - 1
+        while left <= right:
+            mid = (left + right) >> 1
+            if mid + citations[mid] >= n:
+                right = mid - 1
+            else:
+                left = mid + 1                
+        return n - left
