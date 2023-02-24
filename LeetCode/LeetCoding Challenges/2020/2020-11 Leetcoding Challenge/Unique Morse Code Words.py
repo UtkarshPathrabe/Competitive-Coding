c909823cf23a6d43d4920a03a771a2131ada0731
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-02-24 19:38:44 +0530

    Delete Unique Morse Code Words.py

--- LeetCode/LeetCoding Challenges/2020/2020-11 Leetcoding Challenge/Unique Morse Code Words.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        MORSE_CODE = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        return len({''.join(MORSE_CODE[ord(char) - ord('a')] for char in word) for word in words})
