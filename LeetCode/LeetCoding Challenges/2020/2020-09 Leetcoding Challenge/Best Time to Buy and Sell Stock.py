be019d473599d5b559bcaf4e742cc71f109a614f
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-02-24 19:29:42 +0530

    Delete Best Time to Buy and Sell Stock.py

--- LeetCode/LeetCoding Challenges/2020/2020-09 Leetcoding Challenge/Best Time to Buy and Sell Stock.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        maxProfit, minCost = 0, float('inf')
-        for price in prices:
-            minCost = min(minCost, price)
-            maxProfit = max(maxProfit, price - minCost)
-        return maxProfit
