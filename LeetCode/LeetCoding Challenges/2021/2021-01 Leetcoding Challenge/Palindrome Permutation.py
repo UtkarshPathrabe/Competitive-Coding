ec74673b8b98b99efcef6ac9034e8064e4a9fb9e
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-02-24 19:48:39 +0530

    Delete Palindrome Permutation.py

--- LeetCode/LeetCoding Challenges/2021/2021-01 Leetcoding Challenge/Palindrome Permutation.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def canPermutePalindrome(self, s: str) -> bool:
-        characterSet = set()
-        for c in s:
-            if c in characterSet:
-                characterSet.remove(c)
-            else:
-                characterSet.add(c)
-        return len(characterSet) <= 1
