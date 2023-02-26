733a0f60b4194b944f94333ba09468f6be006ca3
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-02-26 11:22:29 +0530

    Delete Custom Sort String.py

--- LeetCode/LeetCoding Challenges/2021/2021-07 Leetcoding Challenge/Custom Sort String.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def customSortString(self, order: str, str: str) -> str:
-        count, result = collections.Counter(str), []
-        for char in order:
-            result.append(char * count[char])
-            count[char] = 0
-        for char in count:
-            result.append(char * count[char])
-        return ''.join(result)
