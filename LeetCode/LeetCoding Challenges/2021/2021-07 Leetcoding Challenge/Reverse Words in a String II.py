02d19544ffd6e81aa1edabce884e99452ca54b7d
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-02-26 11:23:40 +0530

    Delete Reverse Words in a String II.py

--- LeetCode/LeetCoding Challenges/2021/2021-07 Leetcoding Challenge/Reverse Words in a String II.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def reverseWords(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        s[:] = list(' '.join(''.join(s).split()[::-1]))
