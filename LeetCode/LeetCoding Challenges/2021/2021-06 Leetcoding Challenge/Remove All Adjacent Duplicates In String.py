463aacd5f826b6b3aeae817e48065da062379847
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-02-26 11:21:28 +0530

    Delete Remove All Adjacent Duplicates In String.py

--- LeetCode/LeetCoding Challenges/2021/2021-06 Leetcoding Challenge/Remove All Adjacent Duplicates In String.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, S: str) -> str:
-        dequeue = deque()
-        for char in S:
-            if not dequeue or dequeue[-1] != char:
-                dequeue.append(char)
-            else:
-                dequeue.pop()
-        return ''.join(dequeue)
