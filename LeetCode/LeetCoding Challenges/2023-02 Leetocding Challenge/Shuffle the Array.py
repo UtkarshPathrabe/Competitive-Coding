fb36a7e856653cd14b402333d582abc7363389d3
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-03-05 19:24:38 +0530

    Delete Shuffle the Array.py

--- LeetCode/LeetCoding Challenges/2023-02 Leetocding Challenge/Shuffle the Array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def shuffle(self, nums: List[int], n: int) -> List[int]:
-        result = [float('-inf')] * (2 * n)
-        for i in range(n):
-            result[2 * i] = nums[i]
-            result[(2 * i) + 1] = nums[n + i]
-        return result
