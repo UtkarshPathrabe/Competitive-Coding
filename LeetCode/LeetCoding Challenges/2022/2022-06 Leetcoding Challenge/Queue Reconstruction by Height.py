80dab470a68745865f74876d7f26a29945fd52fe
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-03-18 22:46:04 +0530

    Delete Queue Reconstruction by Height.py

--- LeetCode/LeetCoding Challenges/2022/2022-06 Leetcoding Challenge/Queue Reconstruction by Height.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        result = []
-        for person in sorted(people, key = lambda x : (-x[0], x[1])):
-            result.insert(person[1], person)
-        return result
