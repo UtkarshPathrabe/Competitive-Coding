4903ff5c1b77f4315164ed563430d8ff90110327
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-03-18 22:23:39 +0530

    Create Merge Sorted Array.py

---
+++ LeetCode/LeetCoding Challenges/2022-06 Leetcoding Challenge/Merge Sorted Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i, j, k = m - 1, n - 1, m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        if j >= 0:
+            while k >= 0:
+                nums1[k] = nums2[j]
+                j -= 1
+                k -= 1
