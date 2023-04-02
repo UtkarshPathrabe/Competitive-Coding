8bdfe415df0489ed82093cc68a214bc40b9e45d0
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-02 15:41:19 +0530

    Create Increasing Order Search Tree.py

---
+++ LeetCode/LeetCoding Challenges/2020/2020-12 Leetcoding Challenge/Increasing Order Search Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        def inorder(node):
+            if node:
+                yield from inorder(node.left)
+                yield node.val
+                yield from inorder(node.right)
+        result = currentNode = TreeNode(None)
+        for v in inorder(root):
+            currentNode.right = TreeNode(v)
+            currentNode = currentNode.right
+        return result.right
