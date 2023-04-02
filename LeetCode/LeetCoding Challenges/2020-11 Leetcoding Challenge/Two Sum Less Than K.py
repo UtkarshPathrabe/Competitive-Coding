c1378b9be7285dbf008318ab5afd4b4963a7275a
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-02 15:10:43 +0530

    Delete Two Sum Less Than K.py

--- LeetCode/LeetCoding Challenges/2020-11 Leetcoding Challenge/Two Sum Less Than K.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSumLessThanK(self, A: List[int], K: int) -> int:
-        A.sort()
-        result = -1
-        for i in range(len(A)):
-            j = bisect.bisect_left(A, K - A[i], i + 1) - 1
-            if j > i:
-                result = max(result, A[i] + A[j])
-        return result
