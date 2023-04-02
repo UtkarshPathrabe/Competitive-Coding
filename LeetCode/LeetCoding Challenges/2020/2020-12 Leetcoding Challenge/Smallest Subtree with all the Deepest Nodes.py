554cf22d98d88a23d2532c9cf217f63e6c016fa3
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-02 15:42:19 +0530

    Create Smallest Subtree with all the Deepest Nodes.py

---
+++ LeetCode/LeetCoding Challenges/2020/2020-12 Leetcoding Challenge/Smallest Subtree with all the Deepest Nodes.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        # Tag each node with it's depth.
+        depth = {None: -1}
+        def dfs(node, parent = None):
+            if node:
+                depth[node] = depth[parent] + 1
+                dfs(node.left, node)
+                dfs(node.right, node)
+        dfs(root)
+
+        max_depth = max(depth.values())
+
+        def answer(node):
+            # Return the answer for the subtree at node.
+            if not node or depth.get(node, None) == max_depth:
+                return node
+            L, R = answer(node.left), answer(node.right)
+            return node if L and R else L or R
+
+        return answer(root)
