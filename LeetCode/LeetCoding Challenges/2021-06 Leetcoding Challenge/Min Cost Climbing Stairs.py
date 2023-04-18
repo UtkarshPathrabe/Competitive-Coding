16f978eb5a5586a1476fe3455e02d6263bb90c76
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-18 11:32:06 +0530

    Delete Min Cost Climbing Stairs.py

--- LeetCode/LeetCoding Challenges/2021-06 Leetcoding Challenge/Min Cost Climbing Stairs.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        dp = [cost[0], cost[1], 0]
-        for i in range(2, n):
-            dp[i % 3] = cost[i] + min(dp[(i - 1) % 3], dp[(i - 2) % 3])
-        return min(dp[(n - 1) % 3], dp[(n - 2) % 3])
