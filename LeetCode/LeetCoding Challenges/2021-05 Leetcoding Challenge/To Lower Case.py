b8b08548f54ab4e52931744109e1c63741cd57b9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-18 11:31:14 +0530

    Delete To Lower Case.py

--- LeetCode/LeetCoding Challenges/2021-05 Leetcoding Challenge/To Lower Case.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def toLowerCase(self, s: str) -> str:
-        isUpper = lambda x: 'A' <= x <= 'Z'
-        toLower = lambda x: chr(ord(x) | 32)
-        return ''.join([toLower(x) if isUpper(x) else x for x in s])
