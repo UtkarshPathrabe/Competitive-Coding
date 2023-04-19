2f4c639150e3cf246398f270c86f347aac6af376
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-19 17:48:26 +0530

    Create Longest ZigZag Path in a Binary Tree.py

---
+++ LeetCode/April 2023 Leetcoding Challenge/Longest ZigZag Path in a Binary Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        self.pathLength = 0
+        def dfs(node, goLeft, steps):
+            if node:
+                self.pathLength = max(self.pathLength, steps)
+                if goLeft:
+                    dfs(node.left, False, steps + 1)
+                    dfs(node.right, True, 1)
+                else:
+                    dfs(node.left, False, 1)
+                    dfs(node.right, True, steps + 1)
+        dfs(root, False, 0)
+        dfs(root, True, 0)
+        return self.pathLength
