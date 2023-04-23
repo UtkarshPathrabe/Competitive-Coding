7c5db9cf7703cf4938a656d1304ec8307470ffee
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-23 18:49:04 +0530

    Delete Find the Difference.py

--- LeetCode/LeetCoding Challenges/2022-02 Leetcoding Challenge/Find the Difference.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        result = 0
-        for char in s:
-            result ^= ord(char)
-        for char in t:
-            result ^= ord(char)
-        return chr(result)
