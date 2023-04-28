0acae1539f708a44b283ccac0b54f06eaaaa07e4
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-28 21:36:17 +0530

    Create Convert BST to Greater Tree.py

---
+++ LeetCode/LeetCoding Challenges/2022/2022-04 Leetcoding Challenge/Convert BST to Greater Tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.total = 0
+
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is not None:
+            self.convertBST(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.convertBST(root.left)
+        return root
