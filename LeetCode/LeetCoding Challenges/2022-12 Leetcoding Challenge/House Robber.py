1b8fb17b621eacb7eed815dec8dfd0e18a52c7ee
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-30 08:25:06 +0530

    Delete House Robber.py

--- LeetCode/LeetCoding Challenges/2022-12 Leetcoding Challenge/House Robber.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        prevMax, currMax = 0, 0
-        for num in nums:
-            currMax, prevMax = max(prevMax + num, currMax), currMax
-        return currMax
