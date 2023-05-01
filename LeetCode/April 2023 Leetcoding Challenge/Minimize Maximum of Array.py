a048b12d10e88b38cf03e854013858b6bad97fa5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-05-01 20:03:56 +0530

    Delete Minimize Maximum of Array.py

--- LeetCode/April 2023 Leetcoding Challenge/Minimize Maximum of Array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def minimizeArrayValue(self, nums: List[int]) -> int:
-        result, prefixSum = 0, 0
-        for i in range(len(nums)):
-            prefixSum += nums[i]
-            result = max(result, math.ceil(prefixSum / (i + 1)))
-        return result
