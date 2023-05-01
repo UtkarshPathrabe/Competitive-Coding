6b8425850771979977999ae9db3c12b86d9768b9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-05-01 20:04:11 +0530

    Delete Optimal Partition of String.py

--- LeetCode/April 2023 Leetcoding Challenge/Optimal Partition of String.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def partitionString(self, s: str) -> int:
-        charSet, substringCount = set(), 1
-        for char in s:
-            if char in charSet:
-                charSet = set()
-                substringCount += 1
-            charSet.add(char)
-        return substringCount
