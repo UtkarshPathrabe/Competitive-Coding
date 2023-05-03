8f35aba9181d3e57c522b909d6ddee80954d0608
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-05-03 18:49:07 +0530

    Create Find Duplicate Subtrees.py

---
+++ LeetCode/LeetCoding Challenges/2023/2023-02 Leetocding Challenge/Find Duplicate Subtrees.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
+        count = Counter()
+        result = []
+        
+        def dfsEncode(node):
+            if node is None:
+                return '#'
+            else:
+                key = '{},{},{}'.format(node.val, dfsEncode(node.left), dfsEncode(node.right))
+                count[key] += 1
+                if count[key] == 2:
+                    result.append(node)
+                return key
+        
+        dfsEncode(root)
+        return result
