a8e628d52e6452b651128f7a0209a1a5ea0e4554
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-05-03 18:48:07 +0530

    Create N-th Tribonacci Number.py

---
+++ LeetCode/LeetCoding Challenges/2023/2023-01 Leetcoding Challenge/N-th Tribonacci Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        dp = [0 for _ in range(n + 1)]
+        dp[1] = dp[2] = 1
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        return dp[n]
