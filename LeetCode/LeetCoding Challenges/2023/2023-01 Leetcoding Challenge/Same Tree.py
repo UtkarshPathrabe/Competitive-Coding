e3ace2b42421341cb97c363e3d8939969575c086
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-05-03 18:48:24 +0530

    Create Same Tree.py

---
+++ LeetCode/LeetCoding Challenges/2023/2023-01 Leetcoding Challenge/Same Tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
