620063e4caae485f1790724c3987a27247ed77b4
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-01 07:49:45 +0530

    Delete 14-JUL 1218 Longest Arithmetic Subsequence of Given Difference.py

--- LeetCode/LeetCoding Challenges/July 2023 LeetCoding Challenge/14-JUL 1218 Longest Arithmetic Subsequence of Given Difference.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestSubsequence(self, arr: List[int], difference: int) -> int:
-        dp = defaultdict(int)
-        result = 1
-        for num in arr:
-            beforeNum = dp[num - difference]
-            dp[num] = beforeNum + 1
-            result = max(result, dp[num])
-        return result
