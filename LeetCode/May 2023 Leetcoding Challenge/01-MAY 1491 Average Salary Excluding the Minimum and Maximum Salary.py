a61a87248e2e90decfbec4220c6e87435d5466c2
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-06 18:21:13 +0530

    Delete 01-MAY 1491 Average Salary Excluding the Minimum and Maximum Salary.py

--- LeetCode/May 2023 Leetcoding Challenge/01-MAY 1491 Average Salary Excluding the Minimum and Maximum Salary.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def average(self, salary: List[int]) -> float:
-        minSalary, maxSalary, sumSalary = float('inf'), float('-inf'), 0
-        for s in salary:
-            sumSalary += s
-            minSalary = min(minSalary, s)
-            maxSalary = max(maxSalary, s)
-        sumSalary -= (minSalary + maxSalary)
-        return sumSalary / (len(salary) - 2)
