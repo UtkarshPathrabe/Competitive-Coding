a0b69494fa47c28395b44c2882bf489ea9966120
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-24 07:30:42 +0530

    Delete Most Common Word.py

--- LeetCode/Amazon Interview Questions/Most Common Word.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        wordFreq = Counter(re.sub('\W+', ' ', paragraph.lower()).split())
-        banned = set(banned)
-        for word, freq in sorted(wordFreq.items(), key = lambda x : x[1], reverse = True):
-            if word not in banned:
-                return word
