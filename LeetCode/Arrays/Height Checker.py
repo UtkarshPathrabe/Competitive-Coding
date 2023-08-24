1efa38105dfb6d8e70cd09e8d97b8ee4f0b2c123
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-24 07:31:30 +0530

    Delete Height Checker.py

--- LeetCode/Arrays/Height Checker.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        heightsCopy = sorted(list(heights))
-        swapsRequired = 0
-        for i in range(len(heights)):
-            if heights[i] != heightsCopy[i]:
-                swapsRequired += 1
-        return swapsRequired
