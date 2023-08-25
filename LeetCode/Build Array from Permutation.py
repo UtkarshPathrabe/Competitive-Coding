f18c275983ae45860f1e24735c13e819884bbedb
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-25 18:54:02 +0530

    Delete Build Array from Permutation.py

--- LeetCode/Build Array from Permutation.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        result = []
-        for i in range(len(nums)):
-            result.append(nums[nums[i]])
-        return result
