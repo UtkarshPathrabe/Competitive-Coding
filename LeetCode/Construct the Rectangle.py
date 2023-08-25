5b2998ce43215cdf9fac74de16cd1f60c836f0b2
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-25 18:56:25 +0530

    Delete Construct the Rectangle.py

--- LeetCode/Construct the Rectangle.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def constructRectangle(self, area: int) -> List[int]:
-        width = math.floor(math.sqrt(area))
-        while width:
-            if area % width == 0:
-                return [area // width, width]
-            else:
-                width -= 1
