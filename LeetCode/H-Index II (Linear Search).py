459a3377f8284e07e5d91e45772141ec7424191b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:41:55 +0530

    Delete H-Index II (Linear Search).py

--- LeetCode/H-Index II (Linear Search).py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        citations = citations[::-1]
-        for i in range(len(citations)):
-            if citations[i] <= i:
-                return i
-        return len(citations)
