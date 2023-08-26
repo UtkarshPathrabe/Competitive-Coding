8dfd45fb08b9429a6b927060fe15b43a7403fac5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:41:50 +0530

    Delete Greatest English Letter in Upper and Lower Case.py

--- LeetCode/Greatest English Letter in Upper and Lower Case.py
+++
@@ -1,9 +0,0 @@
-from string import ascii_uppercase as auc
-
-class Solution:
-    def greatestLetter(self, s: str) -> str:
-        hashSet = set(s)
-        for char in reversed(auc):
-            if char in hashSet and char.lower() in hashSet:
-                return char
-        return ''
