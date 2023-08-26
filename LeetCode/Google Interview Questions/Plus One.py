f0868c7a52ddfc07f5baed447a55b72392dcfaa8
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:40:45 +0530

    Delete Plus One.py

--- LeetCode/Google Interview Questions/Plus One.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        for index in range(len(digits) - 1, -1, -1):
-            if digits[index] == 9:
-                digits[index] = 0
-            else:
-                digits[index] += 1
-                return digits
-        return [1] + digits
