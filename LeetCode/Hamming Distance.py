2302032805f8de494d09980420a9b40846156d63
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:42:00 +0530

    Delete Hamming Distance.py

--- LeetCode/Hamming Distance.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        xor = x ^ y
-        bitCount = 0
-        for i in range(32):
-            if (xor >> i) & 1 == 1:
-                bitCount += 1
-        return bitCount
