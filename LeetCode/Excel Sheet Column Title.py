c582a7b3dee4affe49b561cb41750e991cc9e1d8
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:31:28 +0530

    Delete Excel Sheet Column Title.py

--- LeetCode/Excel Sheet Column Title.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def convertToTitle(self, n: int) -> str:
-        result = []
-        while n:
-            result.append(chr(ord('A') + ((n - 1) % 26)))
-            n = (n - 1) // 26
-        return ''.join(result[::-1])
