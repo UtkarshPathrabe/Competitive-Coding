de3b037ddd6dc292b1e58a47c5b2644e702be45b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:32:49 +0530

    Delete First Letter to Appear Twice.py

--- LeetCode/First Letter to Appear Twice.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def repeatedCharacter(self, s: str) -> str:
-        hashSet = set()
-        for c in s:
-            if c in hashSet:
-                return c
-            hashSet.add(c)
