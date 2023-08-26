8d34b11174c87b595ab2909fd382b537d0fb2627
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:25:57 +0530

    Delete Diagonal Traverse II.py

--- LeetCode/Diagonal Traverse II.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        elements = []
-        for row in range(len(nums)):
-            for col in range(len(nums[row])):
-                elements.append((row + col, row * -1, nums[row][col]))
-        elements.sort()
-        return [x[2] for x in elements]
