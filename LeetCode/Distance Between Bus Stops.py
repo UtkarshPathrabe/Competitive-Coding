44b7898ccc31d34169c538fefd9b3352d69e5607
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:26:10 +0530

    Delete Distance Between Bus Stops.py

--- LeetCode/Distance Between Bus Stops.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
-        totalDistance, currentDist, i = sum(distance), 0, start
-        while i != destination:
-            currentDist += distance[i]
-            i = (i + 1) % len(distance)
-        return min(currentDist, totalDistance - currentDist)
