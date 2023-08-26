533f9509051ece271d262c3703afdb0f83455842
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:26:24 +0530

    Delete Divide Array Into Equal Pairs.py

--- LeetCode/Divide Array Into Equal Pairs.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def divideArray(self, nums: List[int]) -> bool:
-        freqMap = Counter(nums)
-        for val in freqMap.values():
-            if val % 2 == 1:
-                return False
-        return True
