4a4d32dde22790e6ca6fc80c9b89b551423f50cd
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-26 18:32:25 +0530

    Delete Find Subsequence of Length K With the Largest Sum.py

--- LeetCode/Find Subsequence of Length K With the Largest Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        heap = []
-        for index, num in enumerate(nums):
-            if len(heap) == k:
-                heappushpop(heap, (num, index))
-            else:
-                heappush(heap, (num, index))
-        return [t[0] for t in sorted(heap, key = lambda x : x[1])]
