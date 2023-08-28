986bcbf4c4725b9a87455f123e737059130e0ca2
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-28 06:49:03 +0530

    Delete Keep Multiplying Found Values by Two.py

--- LeetCode/Keep Multiplying Found Values by Two.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def findFinalValue(self, nums: List[int], original: int) -> int:
-        numsSet = set(nums)
-        while original in numsSet:
-            original *= 2
-        return original
