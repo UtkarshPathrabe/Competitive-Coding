a2a97fdaba98f0b3e481bb01464635e52ead9978
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-29 07:24:37 +0530

    Delete Largest Positive Integer That Exists With Its Negative.py

--- LeetCode/Largest Positive Integer That Exists With Its Negative.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findMaxK(self, nums: List[int]) -> int:
-        numsSet = set(nums)
-        result = -1
-        for num in nums:
-            if (-1 * num) in numsSet:
-                result = max(result, abs(num))
-        return result
