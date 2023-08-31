efabd0a40ce1cf3a9edc2fe2826718384b0aa599
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-31 07:07:08 +0530

    Delete Poor Pigs.py

--- LeetCode/LeetCoding Challenges/2020/2020-11 Leetcoding Challenge/Poor Pigs.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
-        states = (minutesToTest // minutesToDie) + 1
-        return math.ceil(math.log(buckets) / math.log(states))
