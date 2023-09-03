a03eb8548b2d090b8d0725f7201775e47cf978be
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-03 21:00:47 +0530

    Delete Best Time to Buy and Sell Stock with Cooldown.py

--- LeetCode/LeetCoding Challenges/2022/2022-12 Leetcoding Challenge/Best Time to Buy and Sell Stock with Cooldown.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        sold, held, reset = float('-inf'), float('-inf'), 0
-        for price in prices:
-            sold, held, reset = held + price, max(held, reset - price), max(reset, sold)
-        return max(sold, reset)
