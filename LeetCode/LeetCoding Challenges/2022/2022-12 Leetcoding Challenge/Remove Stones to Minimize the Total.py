462ea12203fc99c8e7b46961ff88edff08b10ec0
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-04 18:29:54 +0530

    Delete Remove Stones to Minimize the Total.py

--- LeetCode/LeetCoding Challenges/2022/2022-12 Leetcoding Challenge/Remove Stones to Minimize the Total.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minStoneSum(self, piles: List[int], k: int) -> int:
-        heap = [-1 * pile for pile in piles]
-        heapq.heapify(heap)
-        for i in range(k):
-            maxPile = -1 * heapq.heappop(heap)
-            newPile = maxPile - math.floor(maxPile / 2)
-            heapq.heappush(heap, -1 * newPile)
-        return -1 * sum(heap)
