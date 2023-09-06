f1ea4fc2ed8e8190702c5452ba8ad78ad3070484
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-06 08:15:27 +0530

    Delete Kids With the Greatest Number of Candies.py

--- LeetCode/LeetCoding Challenges/2023/2023-04 Leetcoding Challenge/Kids With the Greatest Number of Candies.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        maxCandies = max(candies)
-        result = [False] * len(candies)
-        for i, candy in enumerate(candies):
-            if candy + extraCandies >= maxCandies:
-                result[i] = True
-        return result
