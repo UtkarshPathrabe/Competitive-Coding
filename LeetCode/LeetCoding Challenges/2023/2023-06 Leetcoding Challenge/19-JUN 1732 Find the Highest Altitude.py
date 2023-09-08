7c671cd58a5f5877b0f0fc8f39b69ab0f705e9f9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-08 18:33:18 +0530

    Delete 19-JUN 1732 Find the Highest Altitude.py

--- LeetCode/LeetCoding Challenges/2023/2023-06 Leetcoding Challenge/19-JUN 1732 Find the Highest Altitude.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def largestAltitude(self, gain: List[int]) -> int:
-        currentAltitude, maxAltitude = 0, 0
-        for g in gain:
-            currentAltitude += g
-            maxAltitude = max(maxAltitude, currentAltitude)
-        return maxAltitude
