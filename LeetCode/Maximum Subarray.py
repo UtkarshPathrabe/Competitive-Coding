f050cfdc5d25a0e2a775704a46f76456b5e0a052
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-09 19:57:41 +0530

    Delete Maximum Subarray.py

--- LeetCode/Maximum Subarray.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        currentSum, result = 0, nums[0]
-        for num in nums:
-            currentSum = max(currentSum, 0) + num
-            result = max(result, currentSum)
-        return result
