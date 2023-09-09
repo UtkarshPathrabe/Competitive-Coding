b2c3946212aac531a711b4481564adfeb3228269
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-09 20:05:01 +0530

    Delete Rotate Image.py

--- LeetCode/Microsoft Interview Questions/Rotate Image.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        numberOfRows = numberOfColumns = len(matrix)
-        for i in range(numberOfRows):
-            for j in range(i, numberOfColumns):
-                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-        for i in range(numberOfRows):
-            matrix[i].reverse()
