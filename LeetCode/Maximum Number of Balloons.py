4e52c62637d3b0f30a28e822033892daf5b90463
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-09 19:57:22 +0530

    Delete Maximum Number of Balloons.py

--- LeetCode/Maximum Number of Balloons.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def maxNumberOfBalloons(self, text: str) -> int:
-        freqMap = Counter(text)
-        return min(freqMap['b'], freqMap['a'], freqMap['n'], freqMap['l'] // 2, freqMap['o'] // 2)
