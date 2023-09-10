08a99ff50f4ae10d9bbd7a070b5fa932790c1073
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-10 12:49:20 +0530

    Delete Long Pressed Name.py

--- LeetCode/Mock Interviews/Google/Online Assessment 07/Long Pressed Name.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def isLongPressedName(self, name: str, typed: str) -> bool:
-        for (nameKey, nameList), (typedKey, typedList) in zip_longest(*[groupby(name), groupby(typed)], fillvalue=('', [])):
-            if nameKey != typedKey or len(list(nameList)) > len(list(typedList)):
-                return False
-        return True
