988b6a90dc457b4001390f0aec151d952948d16a
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-11 18:50:46 +0530

    Delete Remove Digit From Number to Maximize Result.py

--- LeetCode/Remove Digit From Number to Maximize Result.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def removeDigit(self, number: str, digit: str) -> str:
-        result = 0
-        for i in range(len(number)):
-            if number[i] == digit:
-                result = max(result, int(number[0:i] + number[i+1:]))
-        return str(result)
