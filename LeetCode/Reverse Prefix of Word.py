95fb9fae524228e4972dfcd7808164ab06882f9b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-11 18:51:28 +0530

    Delete Reverse Prefix of Word.py

--- LeetCode/Reverse Prefix of Word.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def reversePrefix(self, word: str, ch: str) -> str:
-        if ch not in word:
-            return word
-        reqIndex = word.index(ch)
-        return word[:reqIndex + 1][::-1] + word[reqIndex + 1:]
