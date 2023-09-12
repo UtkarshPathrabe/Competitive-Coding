59e8d32f7f5fda5102c72a64f25836e35cd0a21c
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-12 18:47:47 +0530

    Delete Running Sum of 1d Array.py

--- LeetCode/Running Sum of 1d Array.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        result = [nums[0]]
-        for num in nums[1:]:
-            result.append(result[-1] + num)
-        return result
