6a59fe0c2e836cab25d30c7ae5a31b99c1fe3c23
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-12 20:13:26 +0530

    Delete Sort the People.py

--- LeetCode/Sort the People.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        data = [(height, name) for height, name in zip(heights, names)]
-        data.sort(reverse = True)
-        return [name for _, name in data]
