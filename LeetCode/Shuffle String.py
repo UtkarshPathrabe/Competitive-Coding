5c96fea2587a03b447ab9da40d1d45572e23a76f
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-12 20:12:07 +0530

    Delete Shuffle String.py

--- LeetCode/Shuffle String.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def restoreString(self, s: str, indices: List[int]) -> str:
-        length = len(s)
-        result = [''] * length
-        for i in range(length):
-            result[indices[i]] = s[i]
-        return ''.join(result)
