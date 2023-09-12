cec7d15b464524aaee35de207a85efd68192816b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-12 20:13:31 +0530

    Delete Split a String in Balanced Strings.py

--- LeetCode/Split a String in Balanced Strings.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def balancedStringSplit(self, s: str) -> int:
-        frequency, result = 0, 0
-        for char in s:
-            frequency += 1 if char == 'L' else -1
-            if frequency == 0:
-                result += 1
-        return result
