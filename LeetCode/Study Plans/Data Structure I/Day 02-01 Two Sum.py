4ce22c4f53b1892408dc52d468be0727ea0ffd74
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-19 18:24:28 +0530

    Delete Day 02-01 Two Sum.py

--- LeetCode/Study Plans/Data Structure I/Day 02-01 Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = {}
-        for i, num in enumerate(nums):
-            complementNumber = target - num
-            if complementNumber in hashMap:
-                return [hashMap[complementNumber], i]
-            hashMap[num] = i
-        return []
