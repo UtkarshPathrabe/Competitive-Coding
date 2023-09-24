62c4064212a7f11fcd32ef37c263a7e461f0a332
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-24 15:57:17 +0530

    Delete Substrings of Size Three with Distinct Characters.py

--- LeetCode/Substrings of Size Three with Distinct Characters.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def countGoodSubstrings(self, s: str) -> int:
-        result = 0
-        for i in range(len(s) - 2):
-            if len(set(s[i:i+3])) == 3:
-                result += 1
-        return result
