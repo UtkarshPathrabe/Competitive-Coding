c6916020d790a3d222429c8fcb4456b17d270cae
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-28 10:17:51 +0530

    Delete Sum of Unique Elements.py

--- LeetCode/Sum of Unique Elements.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sumOfUnique(self, nums: List[int]) -> int:
-        freqMap = Counter(nums)
-        result = 0
-        for key, value in freqMap.items():
-            if value == 1:
-                result += key
-        return result
