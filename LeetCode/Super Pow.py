7b3dd8f160e3f99ec94d88d4b37026a16244ea1b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-28 10:17:58 +0530

    Delete Super Pow.py

--- LeetCode/Super Pow.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def superPow(self, a: int, b: List[int]) -> int:
-        result, mod = 1, 1337
-        a %= mod
-        for num in b:
-            result = (((result ** 10) % mod) * ((a ** num) % mod)) % mod
-        return result
