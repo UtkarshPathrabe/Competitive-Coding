97a226277efa7192fa854612497c922d190f8cc1
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-09-30 11:37:11 +0530

    Delete Verify Preorder Serialization of a Binary Tree.py

--- LeetCode/Verify Preorder Serialization of a Binary Tree.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isValidSerialization(self, preorder: str) -> bool:
-        nodes = preorder.split(',')
-        slots = 1
-        for node in nodes:
-            if slots <= 0:
-                return False
-            slots += -1 if node == '#' else 1
-        return slots == 0
