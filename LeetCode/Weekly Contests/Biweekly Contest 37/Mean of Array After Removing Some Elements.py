94dd646f6dfb49c0f3acb3d4b11c3c0fb4d7a9e8
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-10-03 06:48:54 +0530

    Delete Mean of Array After Removing Some Elements.py

--- LeetCode/Weekly Contests/Biweekly Contest 37/Mean of Array After Removing Some Elements.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def trimMean(self, arr: List[int]) -> float:
-        arr.sort()
-        arrLength = len(arr)
-        return sum(arr[int(0.05 * arrLength): int(0.95 * arrLength)]) / int(0.9 * arrLength)
