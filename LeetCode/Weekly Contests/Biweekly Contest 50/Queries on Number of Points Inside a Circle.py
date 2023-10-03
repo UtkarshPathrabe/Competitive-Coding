385686978d7e698e5b6efc8d5ea61f33bc7da800
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-10-03 06:49:17 +0530

    Delete Queries on Number of Points Inside a Circle.py

--- LeetCode/Weekly Contests/Biweekly Contest 50/Queries on Number of Points Inside a Circle.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
-        return [len([p for p in points if pow(p[0] - q[0], 2) + pow(p[1] - q[1], 2) <= pow(q[2], 2)]) for q in queries]
