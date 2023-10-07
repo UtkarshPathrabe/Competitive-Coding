703723cbc11ed42316e44e9c7596ec6b46037716
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-10-07 18:31:46 +0530

    Delete Maximum Element After Decreasing and Rearranging.py

--- LeetCode/Weekly Contests/Biweekly Contest 51/Maximum Element After Decreasing and Rearranging.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
-        arr = sorted(arr)
-        if arr[0] != 1:
-            arr[0] = 1
-        for i in range(1, len(arr)):
-            if abs(arr[i] - arr[i - 1]) > 1:
-                arr[i] = arr[i - 1] + 1
-        return arr[-1]
