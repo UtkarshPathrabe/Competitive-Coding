35c88b98a0442c49c7be5e702f732c72759ffa18
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-10-07 18:36:50 +0530

    Delete Maximum Number of Coins You Can Get.py

--- LeetCode/Weekly Contests/Weekly Contest 203/Maximum Number of Coins You Can Get.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxCoins(self, piles: List[int]) -> int:
-        piles.sort(reverse = True)
-        coins, numberOfPiles = 0, len(piles)
-        for i in range(1, numberOfPiles - (numberOfPiles // 3), 2):
-            coins += piles[i]
-        return coins
