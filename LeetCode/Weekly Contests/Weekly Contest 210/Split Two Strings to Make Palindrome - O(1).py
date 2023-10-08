b794bfb224510e0d44e0b9f7b24ab92a0fd539b5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-10-08 18:18:47 +0530

    Delete Split Two Strings to Make Palindrome - O(1).py

--- LeetCode/Weekly Contests/Weekly Contest 210/Split Two Strings to Make Palindrome - O(1).py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def checkPalindromeFormation(self, a: str, b: str) -> bool:
-        if len(a) == 1:
-            return True
-        if a[:2] == b[-2:][::-1]:
-            return True
-        if a[-2:][::-1] == b[:2]:
-            return True
-        return False
