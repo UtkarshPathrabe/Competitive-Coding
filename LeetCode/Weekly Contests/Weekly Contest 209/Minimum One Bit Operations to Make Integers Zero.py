e3b86faf886b7f4b2fc82dedb974259ffe641bd9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-10-08 18:18:28 +0530

    Delete Minimum One Bit Operations to Make Integers Zero.py

--- LeetCode/Weekly Contests/Weekly Contest 209/Minimum One Bit Operations to Make Integers Zero.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def minimumOneBitOperations(self, n: int) -> int:
-        result = 0
-        while n:
-            result = ((n & -n) << 1) - 1 - result
-            n -= n & -n
-        return result
